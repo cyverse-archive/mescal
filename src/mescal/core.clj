67fe147051cd61f417f90e4379b7ddabe98cb608
Dennis Roberts <dennis@iplantcollaborative.org>
2013-09-06 15:19:20 -0700

    added code for job submission

--- src/mescal/core.clj
+++ src/mescal/core.clj
@@ -37,12 +37,66 @@
                {:accept :json
                 :as     :stream})))
 
+(defn- list-my-apps-v1
+  [base-url]
+  (extract-result-v1
+   (client/get (str (curl/url base-url "apps-v1" "apps" "shared" "list"))
+               {:accept :json
+                :as     :stream})))
+
 (defn- get-app-v1
   [base-url app-id]
   (extract-result-v1
    (client/get (str (curl/url base-url "apps-v1" "apps" app-id))
                {:accept :json
                 :as     :stream})))
+
+(defn- required-field
+  [field m]
+  (let [v (m field)]
+    (when (nil? v)
+      (throw (IllegalArgumentException. (str "Missing required field, " (name field)))))
+    v))
+
+(defn- common-params-v1
+  [params]
+  {:softwareName   (required-field :softwareName params)
+   :jobName        (required-field :jobName params)
+   :processorCount (:processorCount params)
+   :maxMemory      (:maxMemory params)
+   :requestedTime  (required-field :requestedTime params)
+   :callbackUrl    (:callbackUrl params)
+   :archive        (:archive params)
+   :archivePath    (:archivePath params)})
+
+(defn- param-value-v1
+  [params param-def]
+  (let [{id :id {:keys [required default]} :value} param-def
+        field-name                                 (keyword id)
+        _                                          (println id)
+        _                                          (println field-name)
+        field-value                                (or (field-name params) default)]
+    (when (and required (nil? field-value))
+      (throw (IllegalArgumentException. (str "Missing required fiedl, " (name field-name)))))
+    [field-name field-value]))
+
+(defn- params-v1
+  [param-defs params]
+  (into {} (map (partial param-value-v1 params) param-defs)))
+
+(defn- submit-job-v1
+  [base-url user token params]
+  (let [form (common-params-v1 params)
+        app  (get-app-v1 base-url (:softwareName form))
+        form (merge form (params-v1 (:inputs app) params))
+        form (merge form (params-v1 (:parameters app) params))
+        form (into {} (remove (fn [[_ v]] (nil? v)) form))]
+    ((comp slurp :body)
+     (client/post (str (curl/url base-url "apps-v1" "job") "/")
+                  {:basic-auth  [user token]
+                   :form-params form
+                   :accept      :json
+                   :as          :stream}))))
 
 (defprotocol AgaveClient
   "A client for the Agave API."
@@ -51,7 +105,8 @@
   (countPublicApps [this])
   (listMyApps [this])
   (countMyApps [this])
-  (getApp [this app-id]))
+  (getApp [this app-id])
+  (submitJob [this params]))
 
 (deftype AgaveClientV1 [base-url user token]
   AgaveClient
@@ -66,9 +121,11 @@
   (countMyApps [this]
     (count (list-my-apps-v1 base-url user token)))
   (getApp [this app-id]
-    (get-app-v1 base-url app-id)))
+    (get-app-v1 base-url app-id))
+  (submitJob [this params]
+    (submit-job-v1 base-url user token params)))
 
-(defn agave-v1-client
+(defn agave-client-v1
   [base-url proxy-user proxy-pass user]
   (let [token (authenticate-v1 base-url proxy-user proxy-pass user)]
     (AgaveClientV1. base-url user token)))
