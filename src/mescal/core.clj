7463aed3f1d2df98ab098d718df7aa9e339337f2
Dennis Roberts <dennis@iplantcollaborative.org>
2013-09-06 16:51:01 -0700

    CORE-4729: refactored the code to clean it up a little

--- src/mescal/core.clj
+++ src/mescal/core.clj
@@ -1,100 +1,5 @@
 (ns mescal.core
-  (:use [clojure.java.io :only [reader]])
-  (:require [cheshire.core :as cheshire]
-            [cemerick.url :as curl]
-            [clj-http.client :as client]))
-
-(defn- decode-json
-  [source]
-  (if (string? source)
-    (cheshire/decode source true)
-    (cheshire/decode-stream (reader source) true)))
-
-(defn- extract-result-v1
-  [response]
-  ((comp :result decode-json :body) response))
-
-(defn- authenticate-v1
-  [base-url proxy-user proxy-pass user]
-  ((comp :token :result decode-json :body)
-   (client/post (str (curl/url base-url "auth-v1") "/")
-                {:accept      :json
-                 :as          :stream
-                 :form-params {:username user}
-                 :basic-auth  [proxy-user proxy-pass]})))
-
-(defn- list-systems-v1
-  [base-url]
-  (extract-result-v1
-   (client/get (str (curl/url base-url "apps-v1" "systems" "list"))
-               {:accept :json
-                :as     :stream})))
-
-(defn- list-public-apps-v1
-  [base-url]
-  (extract-result-v1
-   (client/get (str (curl/url base-url "apps-v1" "apps" "list"))
-               {:accept :json
-                :as     :stream})))
-
-(defn- list-my-apps-v1
-  [base-url]
-  (extract-result-v1
-   (client/get (str (curl/url base-url "apps-v1" "apps" "shared" "list"))
-               {:accept :json
-                :as     :stream})))
-
-(defn- get-app-v1
-  [base-url app-id]
-  (extract-result-v1
-   (client/get (str (curl/url base-url "apps-v1" "apps" app-id))
-               {:accept :json
-                :as     :stream})))
-
-(defn- required-field
-  [field m]
-  (let [v (m field)]
-    (when (nil? v)
-      (throw (IllegalArgumentException. (str "Missing required field, " (name field)))))
-    v))
-
-(defn- common-params-v1
-  [params]
-  {:softwareName   (required-field :softwareName params)
-   :jobName        (required-field :jobName params)
-   :processorCount (:processorCount params)
-   :maxMemory      (:maxMemory params)
-   :requestedTime  (required-field :requestedTime params)
-   :callbackUrl    (:callbackUrl params)
-   :archive        (:archive params)
-   :archivePath    (:archivePath params)})
-
-(defn- param-value-v1
-  [params param-def]
-  (let [{id :id {:keys [required default]} :value} param-def
-        field-name                                 (keyword id)
-        field-value                                (or (field-name params) default)]
-    (when (and required (nil? field-value))
-      (throw (IllegalArgumentException. (str "Missing required fiedl, " (name field-name)))))
-    [field-name field-value]))
-
-(defn- params-v1
-  [param-defs params]
-  (into {} (map (partial param-value-v1 params) param-defs)))
-
-(defn- submit-job-v1
-  [base-url user token params]
-  (let [form (common-params-v1 params)
-        app  (get-app-v1 base-url (:softwareName form))
-        form (merge form (params-v1 (:inputs app) params))
-        form (merge form (params-v1 (:parameters app) params))
-        form (into {} (remove (fn [[_ v]] (nil? v)) form))]
-    ((comp slurp :body)
-     (client/post (str (curl/url base-url "apps-v1" "job") "/")
-                  {:basic-auth  [user token]
-                   :form-params form
-                   :accept      :json
-                   :as          :stream}))))
+  (:require [mescal.agave-v1 :as v1]))
 
 (defprotocol AgaveClient
   "A client for the Agave API."
@@ -109,21 +14,21 @@
 (deftype AgaveClientV1 [base-url user token]
   AgaveClient
   (listSystems [this]
-    (list-systems-v1 base-url))
+    (v1/list-systems base-url))
   (listPublicApps [this]
-    (list-public-apps-v1 base-url))
+    (v1/list-public-apps base-url))
   (countPublicApps [this]
-    (count (list-public-apps-v1 base-url)))
+    (count (v1/list-public-apps base-url)))
   (listMyApps [this]
-    (list-my-apps-v1 base-url user token))
+    (v1/list-my-apps base-url user token))
   (countMyApps [this]
-    (count (list-my-apps-v1 base-url user token)))
+    (count (v1/list-my-apps base-url user token)))
   (getApp [this app-id]
-    (get-app-v1 base-url app-id))
+    (v1/get-app base-url app-id))
   (submitJob [this params]
-    (submit-job-v1 base-url user token params)))
+    (v1/submit-job base-url user token params)))
 
 (defn agave-client-v1
   [base-url proxy-user proxy-pass user]
-  (let [token (authenticate-v1 base-url proxy-user proxy-pass user)]
+  (let [token (v1/authenticate base-url proxy-user proxy-pass user)]
     (AgaveClientV1. base-url user token)))
