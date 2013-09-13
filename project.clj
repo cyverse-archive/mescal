7982815f743b4216f1f16a24dcf8735e9c5166c1
Dennis Roberts <dennis@iplantcollaborative.org>
2013-09-12 18:44:38 -0700

    added a repository specification

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.2.0"]
                  [clj-http "0.7.6"]
-                 [com.cemerick/url "0.1.0"]])
+                 [com.cemerick/url "0.1.0"]]
+  :repositories {"iplantCollaborative"
+                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
