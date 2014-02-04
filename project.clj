33e51f08c998080dcefa043af062bfbd1e68c995
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-04 13:51:59 -0700

    CORE-5133: added some fields that are required by Sonatype to project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,19 @@
 (defproject org.iplantc/mescal "0.1.0-SNAPSHOT"
   :description "A Clojure client library for the Agave API."
   :url "https://github.com/iPlantCollaborativeOpenSource/mescal"
-  :license {:name "BSD Lisence"
+  :license {:name "BSD Standard License"
             :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/kameleon.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/kameleon.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/kameleon.git"}
+  :pom-addition [:developers
+                 [:developer
+                  [:url "https://github.com/orgs/iPlantCollaborativeOpenSource/teams/iplant-devs"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.2.0"]
                  [clj-http "0.7.6"]
                  [com.cemerick/url "0.1.0"]]
-  :repositories {"iplantCollaborative"
-                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]
+                 ["sonatype-nexus-staging"
+                  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]])
